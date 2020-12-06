0a5e70162f3b7353b0a318b0514db6bb6bdd9e7a
Tomoya Tanaka <tomoya113.code@gmail.com>
2020-12-06 14:37:32 +0900

    done#03

--- lib/poll.rb
+++ lib/poll.rb
@@ -1,8 +1,38 @@
 class Poll
-  attr_reader :title, :candidates
+  attr_reader :title, :candidates, :votes, :voters, :deadline
 
-  def initialize(title, candidates)
+  class InvalidCandidateError < StandardError
+  end
+
+  class InvalidVoterError < StandardError
+  end
+
+  class InvalidVoteTimeError < StandardError
+  end
+
+  def initialize(title, candidates, deadline = nil)
     @title = title
     @candidates = candidates
+    @votes = []
+    @voters = []
+    @deadline = deadline
+  end
+
+  def add_vote(vote)
+    unless candidates.include?(vote.candidate)
+      raise InvalidCandidateError
+    end
+
+    if voters.include?(vote.voter)
+      raise InvalidVoterError
+    end
+
+    if deadline != nil && Time.now.to_i > deadline
+      raise InvalidVoteTimeError
+    end
+
+    @votes.push(vote)
+    @voters.push(vote.voter)
+
   end
 end
--- spec/poll_spec.rb
+++ spec/poll_spec.rb
@@ -7,4 +7,42 @@ RSpec.describe Poll do
     expect(poll.title).to eq 'Awesome Poll'
     expect(poll.candidates).to eq ['Alice', 'Bob']
   end
+
+  describe '#add_vote' do
+    it 'saves the given vote' do
+      poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+      vote = Vote.new('Miyoshi', 'Alice')
+
+      poll.add_vote(vote)
+
+      expect(poll.votes).to eq [vote]
+    end
+
+    context 'with a vote that has an invalid candidate' do
+      it 'raises InvalidCandidateError' do
+        poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+        vote = Vote.new('Miyoshi', 'INVALID')
+
+        expect { poll.add_vote(vote) }.to raise_error Poll::InvalidCandidateError
+      end
+    end
+
+    context 'stop multiple attempts' do
+      it 'raises InvalidVoterError' do
+        poll = Poll.new('Awesome Poll', ['Alice', 'Bob'])
+        vote = Vote.new('Miyoshi', 'Alice')
+        poll.add_vote(vote)
+        # 2回目の投票
+        expect { poll.add_vote(vote) }.to raise_error Poll::InvalidVoterError
+      end
+    end
+
+    context 'reject when vote time passed the deadline' do
+      it 'raises InvalidVoteTimeError' do
+        poll = Poll.new('Awesome Poll', ['Alice', 'Bob'], Time.now.to_i - 10)
+        vote = Vote.new('Miyoshi', 'Alice')
+        expect { poll.add_vote(vote) }.to raise_error Poll::InvalidVoteTimeError
+      end
+    end  
+  end
 end
